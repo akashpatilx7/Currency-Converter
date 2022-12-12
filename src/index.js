3fe30faa3371dfad5f14da139999a35bd7b65c55
Akash <akashpatilap876716@gmail.com>
2022-12-12 12:43:53 +0530

    Main Logic For The React-App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Adding the antd Design File into the index.js File
+// import "antd/dist/antd.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
